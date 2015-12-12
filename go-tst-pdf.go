38b5fa1ccfb2923534c0b3e836286891de15bf3b
Ilnur Saifutdinov <ilnursoft@gmail.com>
2015-12-12 21:17:12 +0300

    add ttf fonts

--- go-tst-pdf.go
+++ go-tst-pdf.go
@@ -1,10 +1,30 @@
-// go-tst-pdf
 package main
+  import (
+    "fmt"
+    "github.com/signintech/gopdf"
+  )
 
-import (
-	"fmt"
-)
+  func main() {
 
-func main() {
-	fmt.Println("Hello World!")
-}
+   fmt.Println("start program")
+
+    pdf := gopdf.GoPdf{}
+    pdf.Start(gopdf.Config{Unit: "pt", PageSize: gopdf.Rect{W: 595.28, H: 841.89}}) //595.28, 841.89 = A4
+    pdf.AddPage()
+//    err := pdf.AddTTFFont("HDZB_5", "ttf/wts11.ttf")
+	err := pdf.AddTTFFont("HDZB_5", "ttf/Roboto-Regular.ttf")
+    if err != nil {
+        fmt.Print(err.Error())
+        return
+    }
+
+    err = pdf.SetFont("HDZB_5", "", 4)
+    if err != nil {
+        fmt.Print(err.Error())
+        return
+    }
+	pdf.Cell(nil, "Hello")
+	pdf.Cell(nil, " Привет как дела?")
+    pdf.WritePdf("hello.pdf")
+	fmt.Println("end program")
+  }
